5dc2bbd9060a58feb27c012227904f8c963ae8f5
liyanfeng <yanfeng.li@medishare.cn>
2017-09-23 08:06:35 +0800

    aa

--- prepare_data.py
+++ prepare_data.py
@@ -25,102 +25,69 @@ class ImgCapData(object):
         self.w2idx = None
         self.max_length = max_length
         self.anno_file = anno_file
+        self.vocabs = None
 
     def process_annotations(self):
         with open(self.anno_file) as f:
             anno_data = json.load(f)
 
         data = []
-        for ann in anno_data:
+        image_files = {}
+        vocabs = Counter()
+        for i, ann in enumerate(anno_data):
             image_id = ann['image_id']
+            image_files[image_id] = i
             for cap in ann['caption']:
-                if len(seg(cap)) <= self.max_length:
+                words = seg(cap)
+                if len(words) <= self.max_length:
+                    for w in words:
+                        vocabs[w] += 1
                     data += [{'image':image_id,'caption':cap}]
+
         self.data = data
-        return data
+        self.image_files = image_files
+        self.vocabs = vocabs
 
-    def build_vocabulary(self):
-        counter = Counter()
-        for anno in self.data:
-            words = seg(anno['caption'])
-            for w in words:
-                counter[w] += 1
+    def build_vocabulary_idx(self):
 
-        vocab = [w for w in counter]
-        w2idx = {NULL: 0, START: 1, END: 2}
+        vocab = [w for w in self.vocabs]
+        w2idx = {self.NULL: 0, self.START: 1, self.END: 2}
         idx = 3
         for word in vocab:
             w2idx[word] = idx
             idx += 1
 
         self.w2idx = w2idx
-        return w2idx
 
-    def build_caption_vec(self):
+    def build_img_cap_vec(self):
         n_examples = len(self.data)
         captions = np.ndarray((n_examples,self.max_length+2)).astype(np.int32)
-        
-        for idx, anno in enumerate(annotations):
+        image_idxs = np.ndarray(n_examples, dtype=np.int32)
+
+        for idx, anno in enumerate(self.data):
+            image_idxs[idx] = self.image_files[anno['image']]
             words = seg(anno['caption'])
             vec = []
-            vec.append(w2idx[START])
+            vec.append(self.w2idx[self.START])
             for w in words:
-                vec.append(w2idx[w])
-            vec.append(w2idx(END))
+                vec.append(self.w2idx[w])
+            vec.append(self.w2idx(self.END))
 
             pad_n = self.max_length + 2 - len(vec)
-            vec.extend([w2idx[NULL] for _ in range(0,pad_n)])
+            vec.extend([self.w2idx[self.NULL] * pad_n])
 
             captions[idx,:] = np.asarray(vec)
 
-        self.caption_vecs = captions
-
-        return captions
-
-    def build_image_file(self):
-        image_files = {}
-
-        idx = 0
-        for anno in self.data:
-            img_id = anno['image']
-            if not img_id in image_files:
-                image_files[img_id] = idx
-                idx += 1
-        
-        self.image_files = image_files
-
-    def build_imgage_idx(self):
-        image_idxs = np.ndarray(len(self.data), dtype=np.int32)
-        
-        for i, anno in enumerate(self.data):
-            image_idxs[i] = self.image_files[anno['image']]
-        
         self.image_idx = image_idxs
+        self.caption_vecs = captions
 
 
     def build_all_and_save(self):
-        batch_size = 100
-        max_length = 15
+        self.process_annotations()
+        self.build_vocabulary_idx()
+        self.build_img_cap_vec()
 
-        train_data = process_annotations(basedir+'/train/caption.json',max_length)
-        val_data = process_annotations(basedir+'/val/caption.json',max_length)
-
-        # about 4000 images and 20000 captions for val / test dataset
-        val_cutoff = int(0.1 * len(val_data))
-        test_cutoff = int(0.2 * len(val_data))
-
-        save_pickle(train_data, basedir+'/train/train.annotations.pkl')
-        save_pickle(val_data[:val_cutoff], basedir+'/val/val.annotations.pkl')
-        save_pickle(val_data[val_cutoff:test_cutoff], basedir+'/test/test.annotations.pkl')
-
-        for d in ['train', 'val', 'test']:
-            annotations = load_pickle(basedir+'/%s/%s.annotations.pkl' % (d, d))
-
-            w2idx = build_vocabulary(annotations)
-            save_pickle(w2idx, basedir+'/%s/w2idx.pkl' % d)
-            
-            captions = build_caption_vec(annotations, w2idx, max_length)
-            save_pickle(captions, basedir+'/%s/%s.captions.pkl' % (d, d))
+        
 
     def load_data():
         pass
