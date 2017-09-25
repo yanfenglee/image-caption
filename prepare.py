33fa37bde5a701f4577a3fd09b346d219bca05e0
liyanfeng <yanfeng.li@medishare.cn>
2017-09-25 16:59:28 +0800

    fix

--- prepare.py
+++ prepare.py
@@ -1,14 +1,13 @@
 from img_cap_data import ImgCapData
 
-
 #train data
-# train_data = ImgCapData(basedir='/Users/lyfpcy/ml/aichallenge/train/', anno_file='/caption.json')
+train_data = ImgCapData(basedir='/Users/lyfpcy/ml/aichallenge/train/', anno_file='/caption.json', max_sample = 5000)
 
-# train_data.build_all_and_save()
-# train_data.extract_feature()
+train_data.build_all_and_save()
+train_data.extract_feature()
 
 # val data
-val_data = ImgCapData(basedir='/Users/lyfpcy/ml/aichallenge/val/', anno_file='/caption.json', max_sample = 100)
+val_data = ImgCapData(basedir='/Users/lyfpcy/ml/aichallenge/val/', anno_file='/caption.json', max_sample = 1000)
 
 val_data.build_all_and_save()
 val_data.extract_feature()
