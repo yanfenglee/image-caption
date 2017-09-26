af8b7a3888c953dda97627e0efda993e535dcf90
liyanfeng <yanfeng.li@medishare.cn>
2017-09-26 23:06:17 +0800

    fix

--- prepare.py
+++ prepare.py
@@ -6,7 +6,11 @@ import os
 basedir = os.environ["ML_DATA"] + "/challenge/"
 
 datadir = sys.argv[1]
-sample_num = int(sys.argv[2])
+
+if len(sys.argv) <= 2:
+    sample_num = None
+else:
+    sample_num = int(sys.argv[2])
 
 data = ImgCapData(basedir=basedir+datadir, anno_file='/caption.json', max_sample = sample_num)
 
